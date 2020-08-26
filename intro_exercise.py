7981793bbdde33e8f8b654235c0e1ef8338d2ae7
Moses <moseslee88@gmail.com>
2020-08-26 00:54:40 -0600

    Cleaned up code and comments

--- intro_exercise.py
+++ intro_exercise.py
@@ -12,7 +12,7 @@ import statistics
 # https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
 # It is the daily number of users from a bike share program
 #filename = "C:\Users\mlee013\Documents\GitHub\MSDS600Week1IntroPythonExercisesUploaded\day.csv"
-#df = pd.read_csv('day.csv')
+
 
 df = pd.read_csv("Documents\GitHub\MSDS600Week1IntroPythonExercisesUploaded\day.csv")
 
@@ -26,13 +26,15 @@ print(df.describe())
 
 # Use the examples in the jupyter notebook to help you here.
 # calculate the mean and standard deviation of the hourly data counts (the 'cnt' column)
-# mean
-#Meanie = statistics.mean([4,6,8,1,1,16])
+  
+
 Meanie = df["cnt"].mean()
 print("The mean of the cnt column is ",Meanie)
+
 # standard deviation
 #standdeviation = statistics.stdev([2.5, 3.25, 5.5, 11.25, 11.75])
-standdeviation = df["cnt"].stdev()
+#standdeviation = df["cnt"].stdev()
+standdeviation = df["cnt"].std()
 print("The standard deviation is ",standdeviation)
 
 # plot the counts ('cnt' column)
